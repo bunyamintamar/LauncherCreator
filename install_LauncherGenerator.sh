9de7f268811645c138061c8edf0b5c7581c1b3aa
Bunyamin TAMAR <bunyamintamar@yandex.com>
2021-12-17 16:58:16 +0300

    fix installer

--- install_LauncherGenerator.sh
+++ install_LauncherGenerator.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# wget https://github.com/bunyamintamar/LauncherGenerator.git ~/Desktop/temp_LauncherGenerator
+# cd ~/Desktop/temp_LauncherGenerator
+# chmod +x install_LauncherGenerator.sh
+# ./install_LauncherGenerator.sh
+
 sudo echo -----------------------------
 
 # install libGL
@@ -7,44 +12,39 @@ sudo apt install libgl1-mesa-dev -y
 
 # create application folder
 cd /opt/
+sudo rm -r LauncherGenerator
 sudo mkdir LauncherGenerator
 cd LauncherGenerator
-mkdir lib
+sudo mkdir lib
 
-# download files to temporary folder
-cd ~/Desktop
-mkdir temp_LauncherGenerator
-mkdir 
-ico
-app
-launcher
-libs
+# go into the temporary folder
+cd ~/Desktop/temp_LauncherGenerator
 
 # change mode to executable
-chmod +x LauncherGenerator
+chmod +x ~/Desktop/temp_LauncherGenerator/BUILD/LauncherGenerator
 
 # copy launcher to /usr/share/applications/
-sudo ~/Desktop/temp_LauncherGenerator/Launcher_Generator.desktop /usr/share/applications/
-chmod +x Launcher_Generator.desktop
+chmod +x ~/Desktop/temp_LauncherGenerator/Launcher_Generator.desktop
+sudo  cp ~/Desktop/temp_LauncherGenerator/Launcher_Generator.desktop /usr/share/applications/
 
 # create links
 cd /opt/LauncherGenerator/lib
 sudo cp ~/Desktop/temp_LauncherGenerator/lib/* .
-ln -s libicudata.so.56.1      libicudata.so.56
-ln -s libicudata.so.56        libicudata.so
-ln -s libicui18n.so.56.1      libicui18n.so.56
-ln -s libicui18n.so.56        libicui18n.so
-ln -s libicuuc.so.56.1        libicuuc.so.56
-ln -s libicuuc.so.56          libicuuc.so
-ln -s libQt5Core.so.5.15.2    libQt5Core.so.5.15
-ln -s libQt5Core.so.5.15      libQt5Core.so.5
-ln -s libQt5Core.so.5         libQt5Core.so
-ln -s libQt5Gui.so.5.15.2     libQt5Gui.so.5.15
-ln -s libQt5Gui.so.5.15       libQt5Gui.so.5
-ln -s libQt5Gui.so.5          libQt5Gui.so
-ln -s libQt5Widgets.so.5.15.2 libQt5Widgets.so.5.15
-ln -s libQt5Widgets.so.5.15   libQt5Widgets.so.5
-ln -s libQt5Widgets.so.5      libQt5Widgets.so
+sudo ln -s libicudata.so.56.1      libicudata.so.56
+sudo ln -s libicudata.so.56        libicudata.so
+sudo ln -s libicui18n.so.56.1      libicui18n.so.56
+sudo ln -s libicui18n.so.56        libicui18n.so
+sudo ln -s libicuuc.so.56.1        libicuuc.so.56
+sudo ln -s libicuuc.so.56          libicuuc.so
+sudo ln -s libQt5Core.so.5.15.2    libQt5Core.so.5.15
+sudo ln -s libQt5Core.so.5.15      libQt5Core.so.5
+sudo ln -s libQt5Core.so.5         libQt5Core.so
+sudo ln -s libQt5Gui.so.5.15.2     libQt5Gui.so.5.15
+sudo ln -s libQt5Gui.so.5.15       libQt5Gui.so.5
+sudo ln -s libQt5Gui.so.5          libQt5Gui.so
+sudo ln -s libQt5Widgets.so.5.15.2 libQt5Widgets.so.5.15
+sudo ln -s libQt5Widgets.so.5.15   libQt5Widgets.so.5
+sudo ln -s libQt5Widgets.so.5      libQt5Widgets.so
 
 # remove temporary folder
 sudo rm -r ~/Desktop/temp_LauncherGenerator
@@ -63,3 +63,4 @@ echo 'December 2021'
 echo '-----------------------------'
 echo
 sleep 2
+read
